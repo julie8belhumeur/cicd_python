58b1f3eca8cd838f68d24192e98cbc807e30ab40
julie8belhumeur <belhumeurjulie4@gmail.com>
2025-05-19 16:18:10 +0200

    implémentation des tests et amélioration de la méthode

--- app/convertisseur.py
+++ app/convertisseur.py
@@ -1,3 +1,8 @@
-def addition(a, b):
-    return a+b
-
+def convertir_minutes_heures(total_minutes):
+    signe = "+"
+    if (total_minutes < 0):
+        signe = "-"
+        total_minutes = -total_minutes
+    heures = total_minutes // 60
+    minutes = total_minutes % 60
+    return signe, heures, minutes
--- tests/test_convertisseur.py
+++ tests/test_convertisseur.py
@@ -1,4 +1,11 @@
-def convertir_minutes(total_minutes):
-    heures = total_minutes // 60
-    minutes = total_minutes % 60
-    return heures, minutes
+from cicd_python.app.convertisseur import convertir_minutes_heures
+
+def run_tests():
+    assert convertir_minutes_heures(1) == ("+", 0, 1)
+    assert convertir_minutes_heures(130) == ("+", 2, 10)
+    assert convertir_minutes_heures(60) == ("+", 1, 0)
+    assert convertir_minutes_heures(-61) == ("-", 1, 1)
+    assert convertir_minutes_heures(-132) == ("-", 2, 12)
+
+if __name__ == "__main__":
+    run_tests()
