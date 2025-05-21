bf104985d96c668bf8a1faedf4852855ff3db8b1
julie8belhumeur <belhumeurjulie4@gmail.com>
2025-05-21 03:44:53 +0200

    insertion d'une erreur

--- app/convertisseur.py
+++ app/convertisseur.py
@@ -7,7 +7,7 @@ def convertir_minutes_heures(total_minutes):
     if total_minutes < 0:
         signe = "-"
         total_minutes = -total_minutes
-    heures = total_minutes // 60
+    heures = total_minutes / 60
     minutes = total_minutes % 60
     print(signe, heures, minutes)
     return signe, heures, minutes
